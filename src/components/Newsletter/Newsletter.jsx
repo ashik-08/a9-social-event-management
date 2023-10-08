6be2f33ca06c9410e8b583a96af5babd5560a8e3
ashik-08 <ashikurrahman.8october@gmail.com>
2023-10-08 09:33:36 +0600

    feat: added footer

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -5,8 +5,8 @@ AOS.init();
 
 const Newsletter = () => {
   return (
-    <div className="mt-52" data-aos="fade-down-right" data-aos-offset="200" data-aos-easing="ease-in-out-sine" data-aos-duration="1000">
-      <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32 rounded-lg drop-shadow-lg">
+    <div className="mt-28 lg:mt-52" data-aos="fade-down-right" data-aos-offset="200" data-aos-easing="ease-in-out-sine" data-aos-duration="1000">
+      <div className="relative isolate overflow-hidden bg-blue-gray-800 py-16 sm:py-24 lg:py-32 rounded-lg drop-shadow-lg">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
             <div className="max-w-xl lg:max-w-lg">
@@ -73,7 +73,7 @@ const Newsletter = () => {
           aria-hidden="true"
         >
           <div
-            className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+            className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#65a188] to-[#d45b5b] opacity-80"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,21 +4,21 @@ import Services from "../../components/Services/Services";
 import Slider from "../../components/Slider/Slider";
 import FollowUs from "../../components/FollowUs/FollowUs";
 import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
+  const ourServices = useLoaderData();
 
-    const ourServices = useLoaderData();
-    // console.log(ourServices);
-
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Slider></Slider>
-            <Services eventServices={ourServices}></Services>
-            <FollowUs></FollowUs>
-            <Newsletter></Newsletter>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Slider></Slider>
+      <Services eventServices={ourServices}></Services>
+      <FollowUs></FollowUs>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
